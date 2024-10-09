0a1a0aa8ae19fe672bf686650048226414d95e97
stephens64 <96099094+stephens64@users.noreply.github.com>
2024-10-09 20:59:15 +0000

    Final Commit

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,27 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import { CharacterCard } from "../component/characterCard";
 import { PlanetCard } from "../component/planetCard";
 import { StarshipCard } from "../component/starshipCard";
-
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div data-bs-theme="dark" className="text-center mt-5">
-		<CharacterCard />
-		<PlanetCard />
-		<StarshipCard />
+	<div data-bs-theme="dark" className="container mt-5">
+		{/* Section for Characters */}
+		<h2 className="text-white">Characters</h2>
+		<div className="row">
+			<CharacterCard />
+		</div>
+
+		{/* Section for Planets */}
+		<h2 className="text-white mt-5">Planets</h2>
+		<div className="row">
+			<PlanetCard />
+		</div>
+
+		{/* Section for Starships */}
+		<h2 className="text-white mt-5">Starships</h2>
+		<div className="row">
+			<StarshipCard />
+		</div>
 	</div>
 );
